fc182e71571f5bbc90d354ec58d4f87e289f1e35
Ryan Sauers <rlsauers08@gmail.com>
2025-02-24 21:27:49 -0600

    Refactored congress-admin-campaign.js and implemented creating campaigns

--- admin/partials/campaigns/class-congress-admin-active-campaign.php
+++ admin/partials/campaigns/class-congress-admin-active-campaign.php
@@ -45,7 +45,7 @@ class Congress_Admin_Active_Campaign {
 	private string $name;
 
 	/**
-	 * The level of the campaign ('Federal' or 'State')
+	 * The level of the campaign ('federal' or 'state')
 	 *
 	 * @var string $level
 	 */
@@ -63,7 +63,7 @@ class Congress_Admin_Active_Campaign {
 	 *
 	 * @param int    $id The id of the campaign.
 	 * @param string $name The name of the campaign.
-	 * @param string $level The level of the campaign ('Federal' or 'State').
+	 * @param string $level The level of the campaign ('federal' or 'state').
 	 * @param int    $num_emails The number of emails sent in the campaign.
 	 */
 	public function __construct( int $id, string $name, string $level, int $num_emails ) {
@@ -82,22 +82,35 @@ class Congress_Admin_Active_Campaign {
 		?>
 <div class="congress-card">
 	<div class="congress-card-header">
-		<span><?php echo esc_html( "$this->name ($this->level)" ); ?></span>
+		<span><?php echo esc_html( "$this->name (" . ucwords( $this->level ) . ')' ); ?></span>
 		<button class="congress-campaign-archive button">Archive</button>
 		<button class="congress-campaign-toggle button button-primary"><?php echo esc_html( $editing ? 'Less ^' : 'More >' ); ?></button>
 	</div>
-	<div class="congress-card-body">
+	<div class="congress-card-body<?php echo esc_attr( $editing ? '' : ' congress-hidden' ); ?>">
 		<div class="congress-campaign-pages-container">
-
-			<!-- Links must be in the same order as pages. -->
 			<ul class="congress-nav">
-				<li><a>Edit</a></li>
-				<li><a>Metrics</a></li>
-				<li><a class="congress-active">Email Templates</a></li>
+				<li>
+					<a 
+					href="#<?php echo esc_attr( "congress-campaign-$this->id-edit-page" ); ?>" 
+					>Edit</a>
+				</li>
+				<li>
+					<a 
+					href="#<?php echo esc_attr( "congress-campaign-$this->id-templates-page" ); ?>" 
+					>Email Templates</a>
+				</li>
+				<li>
+					<a 
+					href="#<?php echo esc_attr( "congress-campaign-$this->id-metrics-page" ); ?>" 
+					>Metrics</a>
+				</li>
 				<li></li>
 			</ul>
-			<div class="congress-campaign-page-container congress-hidden">
-				<form id="">
+			<div 
+				id="<?php echo esc_attr( "congress-campaign-$this->id-edit-page" ); ?>" 
+				class="congress-campaign-page-container congress-hidden"
+			>
+				<form class="congress-campaign-edit-form">
 					<h2>Edit Campaign</h2>
 					<div class="congress-form-group">
 						<label>Name:</label>
@@ -105,9 +118,9 @@ class Congress_Admin_Active_Campaign {
 					</div>
 					<div class="congress-form-group">
 						<label>Level:</label>
-						<select>
-							<option>Federal</option>
-							<option>State</option>
+						<select name="level">
+							<option value="federal">Federal</option>
+							<option value="state">State</option>
 						</select>
 					</div>
 					<div class="congress-form-group">
@@ -115,12 +128,18 @@ class Congress_Admin_Active_Campaign {
 					</div>
 				</form>
 			</div>
-			<div class="congress-campaign-page-container congress-hidden">
+			<div
+				id="<?php echo esc_attr( "congress-campaign-$this->id-metrics-page" ); ?>" 
+				class="congress-campaign-page-container congress-hidden"
+			>
 				<div>
 					<p>Graph Goes Here</p>
 				</div>
 			</div>
-			<div class="congress-campaign-page-container">
+			<div
+				id="<?php echo esc_attr( "congress-campaign-$this->id-templates-page" ); ?>" 
+				class="congress-campaign-page-container"
+			>
 				<div style="display: flex; justify-content: space-between;">
 					<div>
 						<input type="text" placeholder="Subject" aria-label="Subject" name="subject"/>
@@ -166,8 +185,8 @@ class Congress_Admin_Active_Campaign {
 	 * Returns an HTML template to be used by JQuery when new representatives are added.
 	 */
 	public static function get_html_template(): void {
-		$template = new Congress_Admin_Active_Campaign( '', '', '', 0 );
-		$template->display( true );
+		$template = new Congress_Admin_Active_Campaign( 0, '', '', 0 );
+		$template->display( false );
 	}
 
 	/**
@@ -176,6 +195,22 @@ class Congress_Admin_Active_Campaign {
 	 * @return array<Congress_Admin_Campaign>
 	 */
 	public static function get_from_db(): array {
-		return array();
+		global $wpdb;
+		$tablename = Congress_Table_Manager::get_table_name( 'campaign' );
+		$result    = $wpdb->get_results( "SELECT * FROM $tablename" ); // phpcs:ignore
+
+		$campaigns = array();
+		foreach ( $result as $campaign_result ) {
+			array_push(
+				$campaigns,
+				new Congress_Admin_Active_Campaign(
+					$campaign_result->id,
+					$campaign_result->name,
+					$campaign_result->level,
+					0,
+				),
+			);
+		}
+		return $campaigns;
 	}
 }
