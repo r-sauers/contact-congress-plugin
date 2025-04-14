04f9199d170a045addb3d1a56cc6308df9093de2
Ryan Sauers <rlsauers08@gmail.com>
2025-04-13 21:31:26 -0500

    created state page architecture

--- admin/class-congress-admin.php
+++ admin/class-congress-admin.php
@@ -100,6 +100,16 @@ class Congress_Admin {
 			$this->version,
 			'all'
 		);
+
+		if ( isset( $_GET['page'] ) && 'congress_state' === $_GET['page'] ) { // phpcs:ignore
+			wp_enqueue_style(
+				$this->congress,
+				plugin_dir_url( __FILE__ ) . 'css/congress-admin-state.css',
+				array(),
+				$this->version,
+				'all'
+			);
+		}
 	}
 
 	/**
@@ -152,6 +162,14 @@ class Congress_Admin {
 				array(
 					'loadTemplateNonce' => $load_template_nonces,
 				),
+			);
+		} elseif ( isset( $_GET['page'] ) && 'congress_state' === $_GET['page'] ) { // phpcs:ignore
+			wp_enqueue_script(
+				$this->congress,
+				plugin_dir_url( __FILE__ ) . 'js/congress-admin-state.js',
+				array( 'jquery' ),
+				$this->version,
+				false
 			);
 		}
 	}
@@ -374,6 +392,16 @@ class Congress_Admin {
 				array( $this, 'congress_rep_page_html' )
 			);
 		}
+		if ( current_user_can( 'congress_manage_states' ) ) {
+			add_submenu_page(
+				'congress',
+				'States',
+				'States',
+				'manage_options',
+				'congress_state',
+				array( $this, 'congress_state_page_html' )
+			);
+		}
 	}
 
 	/**
@@ -459,6 +487,31 @@ class Congress_Admin {
 		<div class="wrap">
 			<?php
 				require_once plugin_dir_path( __FILE__ ) . 'partials/representatives/congress-admin-rep-display.php';
+			?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * States menu callback function.
+	 */
+	public function congress_state_page_html(): void {
+		// check user capabilities.
+		if ( ! current_user_can( 'congress_manage_states' ) ) {
+			?>
+			<h1>State Settings</h1>
+			<p>Whoops! You do not have permissions to manage stats, a website admin must give you one of the following roles:<p>
+			<ul style="list-style: disc; padding-left: 1.5em;">
+				<li>Administrator</li>
+			</ul>
+			<?php
+			return;
+		}
+
+		?>
+		<div class="wrap">
+			<?php
+				require_once plugin_dir_path( __FILE__ ) . 'partials/states/congress-admin-state-display.php';
 			?>
 		</div>
 		<?php
---
+++ admin/partials/states/congress-admin-state-display.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the Campaigns admin page.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Congress
+ * @subpackage Congress/admin/partials
+ */
+
+/**
+ * A component for stacked inputs in forms.
+ */
+require_once plugin_dir_path( __FILE__ ) . '../class-congress-admin-stacked-input.php';
+
+?>
+<div>
+	<h1>State Settings</h1>
+</div>
