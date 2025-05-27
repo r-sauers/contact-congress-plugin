4b757c016e8e8d5de6c3308239d84689c4368f75
Ryan Sauers <rlsauers08@gmail.com>
2025-05-27 00:30:50 -0500

    Fix textdomain mismatch

--- congress.php
+++ congress.php
@@ -20,7 +20,7 @@
  * Author URI:        https://github.com/r-sauers
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       congress
+ * Text Domain:       contact-congress
  * Domain Path:       /languages
  */
 
